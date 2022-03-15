f3daa83e2b24dcb81da9c7c092ead2bd6f6a4654
Aliazar Deneke <aliazardeneke@gmail.com>
2022-03-16 00:24:35 +0300

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - sums 3-5_cous - Entry point
- *
- * Return:  (0)
- */
+  * main - computes and prints the sum of all the multiples of
+  * 3 or 5 below 1024.
+  * Return: Nothing.
+  */
 
 int main(void)
 {
-	int i = 0;
-	int m3 = 3;
-	int m5 = 5;
-	int sum = 0;
-	int lim = 1024;
+	int i, res;
 
-	while (i == 0)
-	{
-
-		if (m3 < lim)
-		{
-			sum += m3;
-			m3 += 3;
-		}
-
-		if (m5 < lim && (m5 % 3) != 0)
-		{
-			sum += m5;
-			m5 += 5;
-		}
-		else if (m5 % 3 == 0)
-		{
-			m5 += 5;
-		}
-
-		if (m3 >= lim && m5 >= lim)
-		{
-			i = 1;
-		}
-	}
-	printf("%d\n", sum);
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+	printf("%d\n", res);
 	return (0);
 }
