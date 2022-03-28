e1fb63b9102e2d3c9c456573f5e185b453ebb8df
Aliazar Deneke <38957699+aliazardeneke@users.noreply.github.com>
2022-03-29 00:37:18 +0300

    pointers_arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * _memset - a function name to impliment task 0s
+ * @s: location to fill
+ * @b: char to fill location with
+ * @n: number of bytes to fill
+ * Return: returns pointer to location filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+*(s + i) = b;
+return (s);
+}
