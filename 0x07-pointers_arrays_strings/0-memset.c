12d26f23e68f14613701d1e61dbd5f562c4a991f
Aliazar Deneke <38957699+aliazardeneke@users.noreply.github.com>
2022-03-30 22:34:04 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * _memset - a function name to impliment task 0s
- * @s: location to fill
- * @b: char to fill location with
- * @n: number of bytes to fill
- * Return: returns pointer to location filled
+ * _puts_recursion - prints a string with recursion
+ * @s: string to print
  */
-char *_memset(char *s, char b, unsigned int n)
+void _puts_recursion(char *s)
 {
-unsigned int i;
-
-for (i = 0; i < n; i++)
-*(s + i) = b;
-return (s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
