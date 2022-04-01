751dd17fb4835c5949366e22a6573f8b1822f301
Aliazar Deneke <38957699+aliazardeneke@users.noreply.github.com>
2022-04-01 22:12:58 +0300

    0x09-static_libraries

--- 0x09-static_libraries/1-args.c
+++ 0x09-static_libraries/1-args.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include "main.h"
-/**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+/*
+ * File: 1-args.c
+ * Auth: Brennan D Baraban
  */
-int main(int argc, __attribute__((unused)) char *argv[])
-{
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-	}
-	printf("%d\n", i - 1);
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
