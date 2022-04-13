403a863f7bdf566c1a8117b5a28f297a6947338c
Aliazar Deneke <38957699+aliazardeneke@users.noreply.github.com>
2022-04-13 20:27:43 +0300

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,52 +1,12 @@
-#ifndef CALC
-#define CALC
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-/**
- *op_add - adds two numbers
- *@a:num1
- *@b:num2
- *Return: a + b
- */
-int op_add(int a, int b);
-
-/**
- *op_sub - substracts two numbers
- *@a:num1
- *@b:num2
- *Return: a - b
- */
-int op_sub(int a, int b);
-
-/**
- *op_mul - multiplies two numbers
- *@a:num1
- *@b:num2
- *Return: a * b
- */
-int op_mul(int a, int b);
-
-/**
- *op_div - divides two numbers
- *@a:num1
- *@b:num2
- *Return: a / b or error if b = 0
- */
-int op_div(int a, int b);
-
-/**
- *op_mod - modeule a to b
- *@a:num1
- *@b:num2
- *Return: a % b
- */
-int op_mod(int a, int b);
-
+#ifndef CALC_H
+#define CALC_H
 
 int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * struct op - Struct op
@@ -60,5 +20,4 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-
-#endif /*CALC*/
+#endif /* CALC_H */
