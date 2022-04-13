b2384e17ee642949575d615c6b541fb981087714
Aliazar Deneke <38957699+aliazardeneke@users.noreply.github.com>
2022-04-13 20:29:24 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,53 +1,30 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+
 /**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
+ * get_op_func -  the function that selects the correct function
+ * to perform the operation asked by the user.
+ * @s: An input char pointer operator passed as argument
+ * Return: a pointer to the function that corresponds to the
+ * operator given as a parameter.
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
